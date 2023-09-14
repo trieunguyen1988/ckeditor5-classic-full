3688a6c37b459764421312594638832b70f1d71e
barryle <lmkhang89@gmail.com>
2023-09-14 11:10:20 +0700

    Removing Title plugin

--- sample/script.js
+++ sample/script.js
@@ -27,7 +27,7 @@ function handleSampleError( error ) {
 
 	const message = [
 		'Oops, something went wrong!',
-		`Please, report the following error on ${ issueUrl } with the build id "2mj00cc6q96t-nohdljl880ze" and the error stack trace:`
+		`Please, report the following error on ${ issueUrl } with the build id "q9gfkw9mg7bh-nohdljl880ze" and the error stack trace:`
 	].join( '\n' );
 
 	console.error( message );
--- src/ckeditor.ts
+++ src/ckeditor.ts
@@ -23,7 +23,7 @@ import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
 import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
 import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
-import { Heading, Title } from '@ckeditor/ckeditor5-heading';
+import { Heading } from '@ckeditor/ckeditor5-heading';
 import { Highlight } from '@ckeditor/ckeditor5-highlight';
 import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
 import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
@@ -156,7 +156,6 @@ class Editor extends ClassicEditor {
 		TableToolbar,
 		TextPartLanguage,
 		TextTransformation,
-		Title,
 		TodoList,
 		Underline,
 		WordCount
